f2aa2d6be7210e9c8538d7b72555406ab2aa17cd
Sanket Puhan <54413046+geek-ninja@users.noreply.github.com>
2021-05-29 19:08:50 +0530

    Add files via upload

---
+++ build/precache-manifest.eec80015b1605cc1f5caaa61662e488a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "014f702f0501695a6c67eddb23031d22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c238ba6585f5175ca1a",
+    "url": "/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "5292624c38312d21f02f",
+    "url": "/static/css/main.923ebd7a.chunk.css"
+  },
+  {
+    "revision": "8c238ba6585f5175ca1a",
+    "url": "/static/js/2.f6720564.chunk.js"
+  },
+  {
+    "revision": "931218552ec36a351f95bbf4f79fdeca",
+    "url": "/static/js/2.f6720564.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5292624c38312d21f02f",
+    "url": "/static/js/main.f69225a0.chunk.js"
+  },
+  {
+    "revision": "0f141f1a15ea4bbbb834",
+    "url": "/static/js/runtime-main.ca98175e.js"
+  }
+]);
